9aac82730572d3248edffd6c6332aced674649d7
Simon Bielman <simon@baroque-creations.com>
2017-05-28 22:42:23 -0700

    qs_stack_last() is no longer an inline function.

--- src/stacks.c
+++ src/stacks.c
@@ -31,6 +31,9 @@ qs_stack_t *qs_stack_new_real (size_t type_size)
    return new;
 }
 
+void *qs_stack_last (qs_stack_t *stack)
+   { return qs_stack_last_n (stack, 0); }
+
 void *qs_stack_last_n (qs_stack_t *stack, int n)
 {
    if (stack->count - n - 1 < 0)
